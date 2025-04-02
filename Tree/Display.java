7acec31c6fde1bd8fb97c9a0d7ac7f1de1c02c9f
Rahul modi <rahulkumarmodi.8294@gmail.com>
2025-04-02 18:58:14 +0530

    Create Display.java

---
+++ Tree/Display.java
@@ -0,0 +1,34 @@
+//Displaying all the nodes  of the tree
+
+class Node{
+    int val;
+    Node left;
+    Node right;
+    Node(int val){
+        this.val=val;
+    }
+}
+public class Display{
+    public static void main(String[] args){
+       Node a = new Node (1);
+       Node b = new Node (41);
+       Node c = new Node (5);
+       Node d = new Node (6);
+       Node e= new Node (7);
+       Node f = new Node (8);
+
+       a.left =b; a.right=c;
+       b.left=d; b.right=e;
+       c.left=f;
+       display(a);
+    }
+
+    private static void display(Node root){
+        if(root==null) return;
+        System.out.print
+        (root.val + " ");
+        display(root.left);
+        display(root.right);
+    }
+} 
+
