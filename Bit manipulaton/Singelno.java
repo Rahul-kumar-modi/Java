4bae5645bce766c24c5bcf3b7e6430601698e031
Rahul modi <rahulkumarmodi.8294@gmail.com>
2025-04-13 11:23:01 +0530

    Bit manipulation

---
+++ Bit manipulaton/Singelno.java
@@ -0,0 +1,11 @@
+public class Singelno {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,4,5,1,5,3};
+        int ans=0;
+        for(int ele : arr){
+            ans ^=ele;
+        }
+
+        System.out.println("The number which has occurance only one time is: " +ans);
+    }
+}
