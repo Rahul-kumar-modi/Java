15fcb8b27fdca33dd8ded7aa95f85d52ec3d3fa4
Rahul modi <rahulkumarmodi.8294@gmail.com>
2025-04-15 23:02:30 +0530

    Bit manipulation

---
+++ Bit manipulaton/MinBitFlip.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class MinBitFlip {
+      public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the 1st number : ");
+        int start = sc.nextInt();
+        System.out.print("Enter the 2nd number : ");
+        int goal = sc.nextInt();
+        int n = start ^ goal;
+        int count=0;
+       for(int i=0;i<=31;i++){
+          if((n & (1<<i))!=0) count++;
+       }
+        System.out.println("The total number of minimum 1 bit required to convert " +start+ " number to "  +goal+ " number is: " +count);
+    }
+}
+
