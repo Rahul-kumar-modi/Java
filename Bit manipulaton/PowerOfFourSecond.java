cfd8054783dfb65d6b33f01b49689fa5d3498ff0
Rahul modi <rahulkumarmodi.8294@gmail.com>
2025-04-17 21:53:43 +0530

    Bit manipulation

---
+++ Bit manipulaton/PowerOfFourSecond.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class PowerOfFourSecond {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int num = sc.nextInt();
+        if (num <= 0) {
+            System.out.println("Wrong number");
+        } else {
+            if (((num & (num - 1)) == 0) && (num % 3 == 1)) {
+                System.out.println("Number " + num + " is power of 4");
+            } else
+                System.out.println("Number " + num + " is not power of 4");
+        }
+    }
+}
