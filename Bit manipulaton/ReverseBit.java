88f05c595d642d74d96ae9fbb7dc560af6d72809
Rahul modi <rahulkumarmodi.8294@gmail.com>
2025-04-17 21:54:27 +0530

    Bit manipulation

---
+++ Bit manipulaton/ReverseBit.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ReverseBit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number:");
+        int n =sc.nextInt();
+        int i=0, j=31;
+        while(i<j){
+            int iMask = (1<<i), jMask = (1<<j);
+            boolean iOn = ((n & iMask)!=0), jOn = ((n & jMask)!=0);
+
+            if(iOn != jOn){
+                n ^= iMask;
+                n ^=jMask;
+            }
+            i++;
+            j--;
+        }
+     System.out.println("Number after reversed is: " +n);
+    }
+}
