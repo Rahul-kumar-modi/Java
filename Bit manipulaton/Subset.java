1bf9a3ba6bd35bad6b7cb6517400de7c38925bfa
Rahul modi <rahulkumarmodi.8294@gmail.com>
2025-04-17 21:55:13 +0530

    Bit manipulation

---
+++ Bit manipulaton/Subset.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class Subset {
+    public static void main(String[] args) {
+        int[] nums= {1,2,3};
+        ArrayList<ArrayList<Integer>>  ans = new ArrayList<>();
+        int n = nums.length;
+        int total = (1<<n);
+        for(int num=0;num<total;num++){
+            ArrayList<Integer> list = new ArrayList<>();
+            for(int bitIdx=0; bitIdx<n; bitIdx++){
+                int mask = (1<<bitIdx);
+                if((num & mask)!=0) list.add(nums[bitIdx]);
+            }
+            ans.add(list);
+        }
+
+        System.out.print("Subsets of a nums array is: ");
+        for(int i=0;i<ans.size();i++){
+            System.out.print(ans.get(i) + ",");
+        }
+    }
+}
