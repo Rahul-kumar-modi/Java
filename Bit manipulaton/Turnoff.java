cd068bf22ec58e393088a37d5ee6ece69132141a
Rahul modi <rahulkumarmodi.8294@gmail.com>
2025-04-20 01:25:26 +0530

    Bit manipulation

---
+++ Bit manipulaton/Turnoff.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Turnoff {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int num = sc.nextInt();
+        System.out.print("Enter kth bit : ");
+        int kthbit = sc.nextInt();
+        num  = num & ~(1<<kthbit);
+        System.out.println("After turn off the kth bit the number become: " +num);
+    }
+}
