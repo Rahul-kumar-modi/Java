626577f456abf873c6f5b1e98f950738d3eefe83
Rahul modi <rahulkumarmodi.8294@gmail.com>
2025-04-21 23:05:02 +0530

    Bit manipulation

--- Bit manipulaton/Subset.java
+++ Bit manipulaton/Subset.java
@@ -15,9 +15,7 @@ public class Subset {
             ans.add(list);
         }
 
-        System.out.print("Subsets of a nums array is: ");
-        for(int i=0;i<ans.size();i++){
-            System.out.print(ans.get(i) + ",");
-        }
+        System.out.print("Subsets of a nums array is: " +ans);
+        
     }
 }
